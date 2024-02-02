71227002daa99f6c8c156f79ea5b83c43efe4652
Gabriel Moya <gabrielmoya123@gmail.com>
2024-02-02 16:32:53 -0300

    Update S3 bucket name in terraform configuration

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket_frontend" {
-  bucket = "frontend-bucket-${data.aws_caller_identity.current.account_id}"
+  bucket = "fullstack-automate-frontend-bucket-${data.aws_caller_identity.current.account_id}"
 
   tags = {
     Description = "Bucket for frontend"
