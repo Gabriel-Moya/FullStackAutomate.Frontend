417d2a82e29648f683eb02f0083fc2a3fe0004c3
Gabriel Moya <gabrielmoya123@gmail.com>
2024-02-03 12:03:56 -0300

    Fix formatting of actions in data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "this" {
       identifiers = ["*"]
     }
 
-    actions   = ["s3:GetObject"]
+    actions = ["s3:GetObject"]
     resources = [
       aws_s3_bucket.bucket_frontend.arn,
       "${aws_s3_bucket.bucket_frontend.arn}/*"
