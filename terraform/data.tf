939c4820e3a2ab50b1ec8991b4370dacac13e991
Gabriel Moya <gabrielmoya123@gmail.com>
2024-02-03 12:02:59 -0300

    Add IAM policy for S3 bucket access

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,1 +1,16 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:GetObject"]
+    resources = [
+      aws_s3_bucket.bucket_frontend.arn,
+      "${aws_s3_bucket.bucket_frontend.arn}/*"
+    ]
+  }
+}
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,15 +9,5 @@ resource "aws_s3_bucket" "bucket_frontend" {
 
 resource "aws_s3_bucket_policy" "bucket_frontend_policy" {
   bucket = aws_s3_bucket.bucket_frontend.id
-  policy = jsonencode({
-    Version = "2024-02-03",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.bucket_frontend.arn}/*",
-      },
-    ],
-  })
+  policy = data.aws_iam_policy_document.this.json
 }
