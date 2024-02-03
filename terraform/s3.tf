3e7d337b921055d2d03c52e012c8b6b67abd4423
Gabriel Moya <gabrielmoya123@gmail.com>
2024-02-03 11:55:00 -0300

    Add S3 bucket policy for frontend bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,3 +6,18 @@ resource "aws_s3_bucket" "bucket_frontend" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_s3_bucket_policy" "bucket_frontend_policy" {
+  bucket = aws_s3_bucket.bucket_frontend.id
+  policy = jsonencode({
+    Version = "2024-02-03",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.bucket_frontend.arn}/*",
+      },
+    ],
+  })
+}
